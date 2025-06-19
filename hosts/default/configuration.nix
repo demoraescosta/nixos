e278638c989980c9146bbea08ae9ce33d5b89182
André de Moraes Costa <an.moraescosta@gmail.com>
2025-06-19 17:30:39 -0300

    57          2025-06-19 17:24:56  25.11.20250617.9e83b64     6.15.2  Unknown                 []              True

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -71,7 +71,6 @@ in {
   # services.desktopManager.plasma6.enable = true;
   programs.hyprland = {
     enable = true;
-    withUWSM = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
