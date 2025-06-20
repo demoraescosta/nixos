1517608bcb6229fb14e1de0f80899afdc6ca9bdf
André de Moraes Costa <an.moraescosta@gmail.com>
2025-06-20 20:54:47 -0300

    84          2025-06-20 20:54:44  25.11.20250619.08f2208     6.15.3  Unknown                 []              True

--- home/home.nix
+++ home/home.nix
@@ -40,6 +40,7 @@
             imagemagick
             qbittorrent
             neovim
+            whatsie
         ];
 
 # Home Manager is pretty good at managing dotfiles. The primary way to manage
