b950f3a9ede427c5b29235fdc2502c87e03957e2
André de Moraes Costa <an.moraescosta@gmail.com>
2025-06-23 01:04:18 -0300

    132         2025-06-23 01:04:17  25.11.20250621.4206c4c  6.15.3  Unknown                 []              True

--- home/home.nix
+++ home/home.nix
@@ -21,15 +21,6 @@
   # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # home.packages = [
-  #     (pkgs.buildEnv {
-  #         name = "scripts";
-  #         paths = [
-  #             ./scripts;
-  #         ];
-  #     })
-  # ];
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
@@ -70,10 +61,10 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".config/rofi/" = {
-      recursive = true;
-      source = dotfiles/rofi;
-    };
+    # ".config/rofi/" = {
+    #   recursive = true;
+    #   source = dotfiles/rofi;
+    # };
   };
 
   # Home Manager can also manage your environment variables through
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,8 +42,6 @@ in {
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
-  # services.blueman.enable = true;
-
   # Enable networking
   networking.networkmanager.enable = true;
 
