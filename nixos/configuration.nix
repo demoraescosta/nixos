18e3120f06ac9e42adf4dbd3057bd558de7fc817
André de Moraes Costa <an.moraescosta@gmail.com>
2025-06-29 21:25:16 -0300

    237         2025-06-29 21:24:34  25.11.20250627.30e2e28  6.15.3  Unknown                 []              True

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,6 +12,7 @@ in {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./nh.nix
     inputs.home-manager.nixosModules.default
   ];
 
@@ -203,6 +204,12 @@ in {
       ];
 
     fontconfig = {
+      antialias = true;
+      subpixel.rgba = "none";
+      cache32Bit = true;
+      hinting.enable = true;
+      hinting.autohint = true;
+
       defaultFonts = {
         sansSerif = [
           "Noto Sans"
